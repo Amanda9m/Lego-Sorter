877dceb1204a03c5f61209643e5372bf0e3db8f4
sama <sama@uwaterloo.ca>
2017-10-23 14:36:57 -0400

    Wrote bare-bones code for colour_detector. Used block_mask data to determine if the pixel includes the block colour. Averaged the RGB values of all pixels including the block. Missing component to compare registered colour values to those of circulating LEGO bricks.

---
+++ source/colour-detector/temp.c
@@ -0,0 +1,29 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdint.h>
+#include "image.h"
+#include "colour.h"
+#include "colour_detector.h"
+
+colour detect_colour(image source, image block_mask) {
+	uint8_t r = 0, g = 0, b = 0;
+	int count = 0;
+	int size = min(source.height*source.width, block_mask.height*block_mask.width);
+
+	for (int j = 0; j < size; j++) {
+		if (block_mask.img[j]/*image number format to be further learned*/) {
+			r = r + source.img[j]/*image number format to be further learned*/;
+			g = g + source.img[j]/*image number format to be further learned*/;
+			b = b + source.img[j]/*image number format to be further learned*/;
+			count++;
+		}
+	}
+
+	r = r / count;
+	g = g / count;
+	b = b / count;
+
+	colour block = { r, g, b };
+
+	return block;
+}
