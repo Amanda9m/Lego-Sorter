74027feea1fc415763f3b543922c46d67ce83dfd
swlynch <swlynch@uwaterloo.ca>
2017-10-23 15:42:49 -0400

    Implemented sobel filter binding for OpenCV
    This adds a sobel_filter method that acts on an image.
    Right now it is really picky about what is passed to
    it and will assert if any of the checkable preconditions
    is violated.

--- source/block-detector/opencv-sobel.cpp
+++ source/block-detector/opencv-sobel.cpp
@@ -1,1 +1,69 @@
 
+// Make sure that all C headers are parsed as C headers
+// otherwise linking will fail
+extern "C" {
+#include "image.h"
+#include "opencv-sobel.h"
+}
+
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/core.hpp"
+
+#include <cstdint>
+#include <cassert>
+#include <cstdlib>
+#include <cstring>
+
+/* This function is heavily inspired by this tutorial
+   https://docs.opencv.org/3.2.0/d2/d2c/tutorial_sobel_derivatives.html	
+*/
+extern "C" sobel_filter_error_code sobel_filter(
+	image source,
+	image* output)
+{
+	assert(source.channels == 1);
+	assert(!!output);
+
+	try {
+		// Create an OpenCV matrix holding the image
+		cv::Mat src = cv::Mat(
+			source.width,
+			source.height,
+			CV_8UC(1),
+			source.img);
+
+		cv::Mat grad_x, grad_y, abs_grad_x, abs_grad_y, grad, out;
+
+		// Get the x and y gradiends
+		cv::Sobel(src, grad_x, CV_16SC1, 1, 0);
+		cv::Sobel(src, grad_y, CV_16SC1, 0, 1);
+
+		cv::convertScaleAbs(grad_x, abs_grad_x);
+		cv::convertScaleAbs(grad_y, abs_grad_y);
+
+		cv::addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0, grad);
+
+		output->width = source.width;
+		output->height = source.height;
+		output->channels = 1;
+
+		grad.assignTo(out, CV_8UC1);
+
+		output->img = (uint8_t*)malloc(
+			source.width * source.height * sizeof(uint8_t));
+
+		if (!output->img)
+			return SOBEL_FILTER_OUT_OF_MEMORY;
+
+		assert(out.isContinuous());
+
+		std::memcpy(output->img, out.data,
+			source.width * source.height * sizeof(uint8_t));
+	} 
+	catch (...)
+	{
+		return SOBEL_FILTER_INTERNAL_ERROR;
+	}
+	return SOBEL_FILTER_SUCCESS;
+}
---
+++ source/block-detector/opencv-sobel.h
@@ -0,0 +1,35 @@
+#ifndef BLOCK_DETECTOR_OPENCV_SOBEL
+#define BLOCK_DETECTOR_OPENCV_SOBEL
+
+#include "image.h"
+
+enum sobel_filter_error_code
+{
+	SOBEL_FILTER_SUCCESS = 0,
+	SOBEL_FILTER_OUT_OF_MEMORY,
+	SOBEL_FILTER_INTERNAL_ERROR
+};
+
+/* Excecutes OpenCV's Sobel function to determine the 
+   sobel filtered image given the source image.
+
+   Preconditions:
+      - source is a greyscale image { channels == 1 }
+	  - output is not null { !!output }
+	  - output is not a valid image
+
+   Postconditions:
+	  - output is overwritten with a new image if the
+	    return code is SOBEL_FILTER_SUCCESS
+	  - source is the same as before
+	  - If the return code is not SOBEL_FILTER_SUCCESS
+	    the state of of output is undefined
+
+   Returns an error code that indicates whether an
+   error occurred while executing the function.
+*/
+enum sobel_filter_error_code sobel_filter(
+	const image source,
+	image* output);
+
+#endif
