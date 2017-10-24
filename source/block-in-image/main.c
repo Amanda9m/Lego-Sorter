95753a3e01fb7df7db58454af89596ac99b21061
swlynch <swlynch@uwaterloo.ca>
2017-10-24 19:51:35 -0400

    Commented block-in-image main
    Added comments to explain what is being done within
    block_in_image

--- source/block-in-image/main.c
+++ source/block-in-image/main.c
@@ -19,14 +19,21 @@ int main(int argc, char** argv)
 	}
 
 	image img, mask = { .img = NULL };
+
+	// Load the given image off disk
 	int err = import_image(argv[1], &img);
 
+	// Check to see if an error occurred while 
+	// loading the image off of disk
 	if (err != 0)
 	{
 		printf("An error occurred while reaing '%s'", argv[1]);
 		return 2;
 	}
 
+	// Use block_in_image to determine whether there 
+	// is a block in the image. Mask is unused because
+	// we will do nothing else other than use result.
 	bool is_brick = block_in_image(img, &mask);
 
 	if (is_brick)
