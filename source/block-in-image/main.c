734d7956913bd714b2ab09b4e4fce5046b9241e3
swlynch <swlynch@uwaterloo.ca>
2017-10-25 18:30:27 -0400

    Made code work

--- source/block-in-image/main.c
+++ source/block-in-image/main.c
@@ -4,6 +4,30 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
+
+image make_grayscale(image source)
+{
+	image out = {
+		source.height,
+		source.width,
+		1,
+		malloc(sizeof(uint8_t) * source.height * source.width)
+	};
+
+	assert(out.img != NULL);
+
+	const size_t size = source.height * source.width;
+	for (size_t i = 0; i < size; ++i)
+	{
+		out.img[i] = (source.img[i * 3]
+			+ source.img[i * 3 + 1]
+			+ source.img[i * 3 + 2]) / 3;
+	}
+
+	return out;
+}
+
 
 // Use a form of main that takes command line arguments
 int main(int argc, char** argv)
@@ -18,7 +42,7 @@ int main(int argc, char** argv)
 		return 1;
 	}
 
-	image img, mask = { .img = NULL };
+	image img, greyscale, mask = { .img = NULL };
 
 	// Load the given image off disk
 	int err = import_image(argv[1], &img);
@@ -31,10 +55,12 @@ int main(int argc, char** argv)
 		return 2;
 	}
 
+	greyscale = make_grayscale(img);
+
 	// Use block_in_image to determine whether there 
 	// is a block in the image. Mask is unused because
 	// we will do nothing else other than use result.
-	bool is_brick = block_in_image(img, &mask);
+	bool is_brick = block_in_image(greyscale, &mask);
 
 	if (is_brick)
 	{
@@ -48,6 +74,7 @@ int main(int argc, char** argv)
 	// Clean up all used resources
 	free(img.img);
 	free(mask.img);
+	free(greyscale.img);
 
 	return 0;
 }
