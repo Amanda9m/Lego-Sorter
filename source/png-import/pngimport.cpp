51c309489f56d203207344ad26d2e769720f1e70
swlynch <swlynch@uwaterloo.ca>
2017-10-25 23:16:06 -0400

    Fix file path slash direction in include directives

--- source/png-import/pngimport.cpp
+++ source/png-import/pngimport.cpp
@@ -2,8 +2,8 @@ extern "C" {
 #include "pngimport.h"
 }
 
-#include "opencv2\core.hpp"
-#include "opencv2\highgui.hpp"
+#include "opencv2/core.hpp"
+#include "opencv2/highgui.hpp"
 
 #include <cstdlib>
 
