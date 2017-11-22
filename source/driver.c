bb299153addf23786353c48793595bc59363f35f
Amanda Morin <asmmorin@edu.uwaterloo.ca>
2017-11-21 22:03:43 -0500

    Adding new set management code, and an overall driver. Note: set managemtn is not complete and will not properly returmn an array, will try to work on that. also driver still contains some pseudocode and will NOT compile yet. needs to be edited and have the function it's calling be edited.

---
+++ source/driver.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+#include "block_detector.h"
+#include "block_recognizer.h"
+#include "colour_detector.h"
+#include "pngimport.h"
+#include "set_management.h"
+
+// this is the driver which is to be run over and over again until the list is full or we stop
+
+int main (void)
+{
+	array = set_management(user_input_document); //this is not at all how i call this and i'm not sure i'll get it, need to work on getting this array.
+	bool blocks_are_left = true;
+	while(blocks_are_left)
+	{
+		image source_img = //call image from camera
+		if(block_in_image(source_img))
+		{
+			block_type type = recognize_block(gray);
+			lego_colour colour = detect_colour(img); //make this mean colour id as in my look up table or change my lookup table to right ids
+			for(int i = 0; i < n; i++) //need to get n from array, need to output that from function too oopssssss MAKE THAT HAPPEN
+			{
+				if(array[i].blocks_left > 0)
+				{
+					if(array[i].length == type.length && array[i].width == type.width
+						&& array[i].thickness == type.thickness && array[i].colour == colour)
+					{
+						inSet(1)//turn green light on
+						// wait 3 seconds???
+						array[i].blocks_left --;
+						i = n;
+					}
+				}
+			}
+		}
+	}
+}
+
+
+//input user files
+
+//call tensorflow block detect
+
+//if yes, continue, if no, try again
+
+//call tensorflow block type and color type
+
+//compare tensorflow block type and user input, color and user input
+
+//if yes, green light, if no, red
+
+
