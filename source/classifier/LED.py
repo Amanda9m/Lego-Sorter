bf72317c8a32862d2b7bba48efb68e7f7e165498
sama <sama@edu.uwaterloo.ca>
2017-11-22 20:18:10 -0500

    Renumbered output pins after catching a mistake in the pin setup and the pinp numbers used in the code.

--- source/classifier/LED.py
+++ source/classifier/LED.py
@@ -9,30 +9,30 @@ def inSet( clr ):
     "This function will flash a green or red light if the block is or is not in the set respectively"
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
-    GPIO.setup(29, GPIO.OUT)
-    GPIO.setup(31, GPIO.OUT)
-    GPIO.setup(33, GPIO.OUT)
+    GPIO.setup(5, GPIO.OUT)
+    GPIO.setup(6, GPIO.OUT)
+    GPIO.setup(13, GPIO.OUT)
 
     if clr==1:
         # Turn the LED green
-        GPIO.output(29, GPIO.LOW)
-        GPIO.output(31, GPIO.LOW)
-        GPIO.output(33, GPIO.HIGH)
+        GPIO.output(5, GPIO.LOW)
+        GPIO.output(6, GPIO.LOW)
+        GPIO.output(13, GPIO.HIGH)
     elif clr==2:
         # Turn the LED red
-        GPIO.output(29, GPIO.HIGH)
-        GPIO.output(31, GPIO.LOW)
-        GPIO.output(33, GPIO.LOW)
+        GPIO.output(5, GPIO.HIGH)
+        GPIO.output(6, GPIO.LOW)
+        GPIO.output(13, GPIO.LOW)
     else:
         # Default: turn the LED blue if input was recieved but invalid
-        GPIO.output(29, GPIO.LOW)
-        GPIO.output(31, GPIO.HIGH)
-        GPIO.output(33, GPIO.LOW)
+        GPIO.output(5, GPIO.LOW)
+        GPIO.output(6, GPIO.HIGH)
+        GPIO.output(13, GPIO.LOW)
 
     
-    GPIO.output(29, GPIO.LOW)
-    GPIO.output(31, GPIO.LOW)
-    GPIO.output(33, GPIO.LOW)
+    GPIO.output(5, GPIO.LOW)
+    GPIO.output(6, GPIO.LOW)
+    GPIO.output(13, GPIO.LOW)
     GPIO.cleanup()
     return;
 
